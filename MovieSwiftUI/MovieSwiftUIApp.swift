5b921cd7bd9e37120414da75631c3ff233698274
KellyR <kellyruiz.dev@gmail.com>
2026-08-08 01:38:09 -0400

    Initial Commit

---
+++ MovieSwiftUI/MovieSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftUIApp.swift
+//  MovieSwiftUI
+//
+//  Created by Kelly Ruiz on 8/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
